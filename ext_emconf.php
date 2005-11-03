5039b25391bc72defd302f1f669f613230b840e1
julle <julle@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-03 15:30:18 +0000

    * Allright... getting back to CVS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sys_todos/trunk@2119 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sys_todos"
 # 
-# Auto generated 16-11-2004 13:55
+# Auto generated 30-09-2005 09:53
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -17,21 +17,26 @@ $EM_CONF[$_EXTKEY] = Array (
 	'dependencies' => 'taskcenter',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '-',
+	'PHP_version' => '-',
 	'module' => '',
-	'state' => 'stable',
+	'state' => 'beta',
 	'internal' => 0,
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'Kasper Skårhøj',
-	'author_email' => 'kasper@typo3.com',
-	'author_company' => 'Curby Soft Multimedia',
+	'author' => 'Christian Jul Jensen',
+	'author_email' => 'julle@typo3.org',
+	'author_company' => 'BEE3 ApS (http://BEE3.com)',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '1.0.6',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:7:{s:23:"#class.tx_systodos.php#";s:4:"2445";s:21:"class.tx_systodos.php";s:4:"0d09";s:12:"ext_icon.gif";s:4:"5c27";s:14:"ext_tables.php";s:4:"5a63";s:14:"ext_tables.sql";s:4:"8336";s:13:"locallang.php";s:4:"4092";s:12:"doc/TODO.txt";s:4:"a997";}',
+	'version' => '2.0.2',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:14:{s:21:"class.tx_systodos.php";s:4:"46e8";s:27:"class.tx_systodos.php.~1.3~";s:4:"37fe";s:12:"ext_icon.gif";s:4:"5c27";s:15:"ext_php_api.dat";s:4:"84a6";s:14:"ext_tables.php";s:4:"5a63";s:14:"ext_tables.sql";s:4:"1ba0";s:13:"locallang.php";s:4:"25cc";s:11:"CVS/Entries";s:4:"5f2f";s:14:"CVS/Repository";s:4:"bc10";s:8:"CVS/Root";s:4:"e21b";s:12:"doc/TODO.txt";s:4:"a997";s:15:"doc/CVS/Entries";s:4:"3c28";s:18:"doc/CVS/Repository";s:4:"4cf5";s:12:"doc/CVS/Root";s:4:"e21b";}',
 );
 
 ?>
